1edc13ba9dc3eea8b12aa9dbc962b7e560e05d16
shyndman <scotty.hyndman@gmail.com>
2012-02-19 11:02:36 -0500

    We now fill in the rdio album info on an AlbumInfo through an Rdio search

--- lib/models.rb
+++ lib/models.rb
@@ -1,11 +1,12 @@
 # Contains information related to an album
 class AlbumInfo
-  attr :artist_name, :album_name, :score
+  attr_accessor :artist_name, :album_name, :score, :pub_date, :rdio_album
 
-  def initialize artist_name, album_name, score
+  def initialize artist_name, album_name, score, pub_date
     @artist_name = artist_name
     @album_name = album_name
     @score = score
+    @pub_date = pub_date
   end
 
   def to_s
--- lib/rdio_service.rb
+++ lib/rdio_service.rb
@@ -1,3 +1,22 @@
- def decorate_with_rdio_info albums
+class RdioService
 
+  RDIO_ACCESS_TOKEN = ".rdio_access_token"
+
+  def initialize
+    token = Marshal.load File.new RDIO_ACCESS_TOKEN
+  end
+
+  def search_for_album album_info
+    Search.search "#{album_info.artist} #{album_info.album_name}", "Album"
+  end
+end
+
+# Searches for each of the provided albums on Rdio, filling out the
+# AlbumInfo.rdio_album attribute if found.
+def decorate_with_rdio_info albums
+  service = RdioService.new
+
+  albums.each do |album_info|
+    album_info.rdio_album = service.search_for_album(album_info)
+  end
 end
