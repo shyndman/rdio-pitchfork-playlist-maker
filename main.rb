f67e66b862470f0d26bf52e3e31cf76e2fd97672
shyndman <scotty.hyndman@gmail.com>
2012-02-19 12:16:34 -0500

    Main rb file now performs startup check

--- main.rb
+++ main.rb
@@ -5,35 +5,39 @@ require "bundler/setup"
 require "open-uri"
 require "nokogiri"
 require "rdio"
+require "launchy"
+require "oauth"
 require "ap"
 
+require "lib/config"
 require "lib/models"
-require "lib/pitchfork_scrape"
+require "lib/pitchfork"
 require "lib/filters"
 require "lib/rdio_service"
 
 
 CONFIG_FILE = "config.yml"
 PAGE_RANGE = 1..1
-MINIMUM_SCORE = 5.0
+SCORE_RANGE = 5.0..10.0
 
 
-# Loads, parses and returns the config.yml file
-def load_config
-  YAML::load(open(CONFIG_FILE))
-end
+# Make sure we have the files we need to
+perform_startup_check
 
 # Scrape the albums from Pitchfork
 albums = get_albums_from_pages PAGE_RANGE
 
 # Filter them by score
-albums = filter_by_score albums, MINIMUM_SCORE
+albums = filter_by_score albums, SCORE_RANGE
 
 # Associate them with Rdio information
 decorate_with_rdio_info albums
 
 # Filter them by Rdio availability
-albums = filter_by_rdio_availability albums
+albums, missing_albums = filter_by_rdio_availability albums
+
+# Create a new playlist
+
 
 # Create the Rdio playlist
 ap albums
