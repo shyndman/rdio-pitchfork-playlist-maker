3be4df1387c50e5a6e6bf61d6066e939d6894d07
shyndman <scotty.hyndman@gmail.com>
2012-02-19 12:15:04 -0500

    Rdio availability filter now returned both the found and missed albums

--- lib/filters.rb
+++ lib/filters.rb
@@ -1,9 +1,19 @@
-def filter_by_score albums, min_score
+def filter_by_score albums, score_range
   albums.select do |album_info|
-    album_info.score >= min_score
+    score_range.include? album_info.score
   end
 end
 
-def filter_by_availability albums
-  albums
+def filter_by_rdio_availability albums
+  separated_albums = { :found => [], :missing => [] }
+
+  albums.each do |album_info|
+    if album_info.rdio_album.nil? or !album_info.rdio_album.can_stream
+      separated_albums[:missing] << album_info
+    else
+      separated_albums[:found] << album_info
+    end
+  end
+
+  [separated_albums[:found], separated_albums[:missing]]
 end
